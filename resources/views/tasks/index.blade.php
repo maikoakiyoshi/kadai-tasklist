c0ef319971415cc511741af0d82f976243de1984
koko <koko_peace@hotmail.com>
2019-01-17 07:04:32 +0000

    CRUD tasks

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')<!--layout.appを共通で呼び出す -->
+
+@section('content')<!--layout.appの@yield('content')に埋め込まれる -->
+
+    <h1>タスク一覧</h1>
+    
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif 
+    
+    {!! link_to_route('tasks.create', 'タスクの追加', null, ['class' => 'btn btn-secondary']) !!}
+@endsection
